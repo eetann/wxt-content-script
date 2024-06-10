0abafbf3afa65af52194ebfa7820f591e8429272
eetann <hideharu092@gmail.com>
2024-06-10 16:46:31 +0900

    chore: change name

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,16 +1,20 @@
-import { defineConfig } from 'wxt';
-import { svelte, vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { defineConfig } from "wxt";
+import { svelte, vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 // See https://wxt.dev/api/config.html
 export default defineConfig({
-  srcDir: 'src',
-  vite: () => ({
-    plugins: [
-      svelte({
-        // Using a svelte.config.js file causes a segmentation fault when importing the file
-        configFile: false,
-        preprocess: [vitePreprocess()],
-      }),
-    ],
-  }),
+	manifest: {
+		name: "wxt-content-script",
+		version: "1.0.0",
+	},
+	srcDir: "src",
+	vite: () => ({
+		plugins: [
+			svelte({
+				// Using a svelte.config.js file causes a segmentation fault when importing the file
+				configFile: false,
+				preprocess: [vitePreprocess()],
+			}),
+		],
+	}),
 });
